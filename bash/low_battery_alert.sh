a7ce624d89e7bd5aaf67fefbbcbc125b0834f43a
arungeorgesaji <arungeorgesaji@gmail.com>
2024-04-13 19:48:43 +0530

    Fixed up the low battery alert

--- bash/low_battery_alert.sh
+++ bash/low_battery_alert.sh
@@ -5,10 +5,10 @@ while true; do
     #Get battery percentage
     battery_percentage=$(upower -i $(upower -e | grep BAT) | awk '/percentage/ {gsub(/%/, "", $2); print $2}')
 
-    #Check if the battery percentage is less than 10 in that case alert the user that that    #the bettery is extremely low,if its not less than 10 if its less than 20 in that case    #alert the user that the battery is low
-    if [[ $battery_percentage -le 100 ]]; then
+    #Check if the battery percentage is less than 10 in that case alert the user that that    #the bettery is extremely low,if its not less than 10,check if its less than 20 and in    #that case alert the user that the battery is low
+    if [[ $battery_percentage -le 10 ]]; then
         dunstify "Your battery is extremely low" -u critical
-    elif [[ $battery_percentage -le 100 ]]; then
+    elif [[ $battery_percentage -le 20 ]]; then
         dunstify "Your battery is low" -u critical
     fi
 
