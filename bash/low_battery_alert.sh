c8318719c0a80ba7df24baf8a0de25ee0abcd011
arungeorgesaji <arungeorgesaji@gmail.com>
2024-04-19 16:53:29 +0530

    Fixed lf file permissions issues and also fixed issue when dunst notifies about low battery even when battery is being charged

--- bash/low_battery_alert.sh
+++ bash/low_battery_alert.sh
@@ -5,14 +5,20 @@ while true; do
     #Get battery percentage
     battery_percentage=$(upower -i $(upower -e | grep BAT) | awk '/percentage/ {gsub(/%/, "", $2); print $2}')
 
-    #Check if the battery percentage is less than 10 in that case alert the user that that    #the bettery is extremely low,if its not less than 10,check if its less than 20 and in    #that case alert the user that the battery is low
-    if [[ $battery_percentage -le 10 ]]; then
+    #Check if the laptop is charging
+    if acpi -a | grep -q 'on-line'; then
+        :
+
+    #Check if the battery is less than 10% and if it is notify user that their battery is     #extremely low
+    elif [[ $battery_percentage -le 10 ]]; then
         dunstify "Your battery is extremely low" -u critical
+
+    #Check if the battery is less than 10% and if it is notify user that their battery is     #low
     elif [[ $battery_percentage -le 20 ]]; then
         dunstify "Your battery is low" -u critical
     fi
 
-    # Sleep for 30 seconds before checking again
-    sleep 30
+    # Sleep for 1 minute before checking again
+    sleep 60
 done
 
