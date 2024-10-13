b43b68c0bd5d53e60336444ff3c923cbd25d5b3d
arungeorgesaji <arungeorgesaji@gmail.com>
2024-10-13 22:50:42 +0530

    Added Copilot to neovim

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,5 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map('i', '<C-l>', function ()
+  vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+end, { desc = 'Copilot Accept', noremap = true, silent = true })
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -6,6 +6,16 @@ return {
       require "configs.conform"
     end,
   },
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+        -- Disable default Tab mapping for Copilot
+        vim.g.copilot_no_tab_map = true
+        vim.g.copilot_assume_mapped = true
+        vim.g.copilot_tab_fallback = ""
+    end,
+  },
 
   -- These are some examples, uncomment them if you want to see them work!
   -- {
